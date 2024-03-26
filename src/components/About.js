0ee3502787dc9f93e117c93b95ffd8674d4d58bb
Sanjay Rautela <140192191+sanjayrautela@users.noreply.github.com>
2024-03-26 07:27:33 +0000

    solution

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+// About.js
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>This is a simple React application demonstrating basic routing capabilities using React Router.</p>
+    </div>
+  );
+};
+
+export default About;
--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,35 @@
-
-import React from "react";
-import './../styles/App.css';
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
 
-export default App
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
